d6e3ea29f48c5cd8a47b993759d7f5ce454e4459
VICTOR HUGO CERQUEIRA SILVA <25162461@edu119.local>
2025-12-05 10:31:34 -0300

    First Commit

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,49 +1,53 @@
-console.log("App.js funcionando")
+console.log("App.js funcionando");
+
 const API = "http://localhost:3000/lerveiculos";
-const APIPagamento = "http://localhost:3000/atualizarpagamento"
+const APIPagamento = "http://localhost:3000/atualizarpagamento";
+const APIDeletar = "http://localhost:3000/deletar";
 
 async function carregar() {
     const res = await fetch(API);
     const dados = await res.json();
 
-    const tabela = document.getElementById("tabela");
-
+    const tabela = document.getElementById("tabela-veiculos"); // CORRIGIDO!!!
     tabela.innerHTML = "";
-    console.log(dados)
 
     dados.forEach((carro) => {
         tabela.innerHTML += `
-    <tr>
-        <td>${carro.id}</td>
-        <td>${carro.placa}</td>
-        <td>${carro.modelo}</td>
-        <td>${carro.pago ? "✅Sim" : "❌Não"}</td>
-        <td>
-            <button onclick="pagar(${carro.id}, ${carro.pago})">
-                  ${carro.pago ? '<span style="color:blue">cancelar</span>' : '<span style="color:green">pagar</span>'}
-            </button>
-             <button onclick="Deletar(${carro.id}, ${carro.pago})">
-                  ${carro.pago ? '<span style="color:blue">cancelar</span>' : '<span style="color:green">pagar</span>'}
-            </button>
-        </td>
-    </tr>
-    `
-    })
+            <tr>
+                <td>${carro.id}</td>
+                <td>${carro.placa}</td>
+                <td>${carro.modelo}</td>
+                <td>${carro.pago ? "✅ Sim" : "❌ Não"}</td>
+                <td>
+                    <button onclick="pagar(${carro.id}, ${carro.pago})">
+                        ${carro.pago ? "Cancelar" : "Pagar"}
+                    </button>
 
+                    <button onclick="Deletar(${carro.id})" style="color:red;">
+                        Excluir
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
 }
 
 async function pagar(id, pagoAtual) {
-    console.log(id)
-    console.log(pagoAtual)
     await fetch(`${APIPagamento}/${id}`, {
         method: "PATCH",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({ pago: !pagoAtual })
-    })
+    });
+
+    carregar();
+}
+
+async function Deletar(id) {
+    await fetch(`${APIDeletar}/${id}`, {
+        method: "DELETE"
+    });
+
     carregar();
 }
 
 carregar();
-//------------------------------------------
-carregar();
-
